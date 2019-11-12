142f124e0397283754fa4323ffab216fee4e616d
laazy <616408874@qq.com>
2019-11-12 15:57:05 +0000

    remove redundancy

--- poker_test.py
+++ poker_test.py
@@ -1,6 +1,5 @@
 import unittest
-from unittest import TestLoader, TestSuite, TextTestRunner
 
 
 if __name__ == "__main__":
-    TextTestRunner().run(TestSuite(TestLoader().discover(start_dir="test")))
+    unittest.TextTestRunner().run(unittest.TestSuite(unittest.TestLoader().discover(start_dir="test")))
