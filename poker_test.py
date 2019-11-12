d11f72ea8616bd2dd4131830ec209dd5f3678605
laazy <616408874@qq.com>
2019-11-12 16:04:03 +0000

    enhance frame to support same name structure

--- poker_test.py
+++ poker_test.py
@@ -2,4 +2,6 @@ import unittest
 
 
 if __name__ == "__main__":
-    unittest.TextTestRunner().run(unittest.TestSuite(unittest.TestLoader().discover(start_dir="test")))
+    unittest.TextTestRunner(verbosity=2).run(
+        unittest.TestSuite(
+            unittest.TestLoader().discover(start_dir="test", top_level_dir=".")))
