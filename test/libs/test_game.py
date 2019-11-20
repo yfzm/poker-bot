fb44c063fe198d5f341e66173ca31b677b93dbf3
Li Hao <yfzm1223@sjtu.edu.cn>
2019-11-20 20:01:32 +0800

    add test

--- test/libs/test_game.py
+++ test/libs/test_game.py
@@ -1,47 +1,45 @@
 import unittest
-from libs.game import Game
+from libs.game import Game, GameStatus, RoundStatus
+from libs.player import Player
 import sys
 import os
 import time
 
 class TestGameKernel(unittest.TestCase):
+    
+    def setUp(self):
+        self.g = Game()
+        self.p1 = Player("u1")
+        self.p2 = Player("u2")
+        self.p3 = Player("u3")
+        self.g.start([self.p1, self.p2, self.p3], 20, 1)
 
+    def test_fold(self):
+        game = self.g
+        self.assertEqual(game.exe_pos, 1)
+        self.assertEqual(game.pfold(1), 0)
+        self.assertEqual(game.get_active_player_num(), 2)
+        self.assertEqual(game.exe_pos, 2)
+        
+        self.assertEqual(game.pcall(2), 0)
+        self.assertEqual(game.pcheck(0), 0)
+        self.assertEqual(game.exe_pos, 2)
+        self.assertEqual(game.pfold(2), 0)
+        self.assertEqual(game.roundStatus, RoundStatus.END)
+        self.assertEqual(game.game_status, GameStatus.WAITING)
 
-# TODO: need add test from game logic
-# action is str like 'BET' 'CALL', playerPos is an integer which show the pos of a player
-# body is an dict obj.
-    def checkHook(self,game, action, playerPos, body):
-        print("action: {}, player: {}, body: {} \n".format(action, playerPos, body))
+    def test_all_in(self):
+        pass
 
-    a = Game(6)
+    def test_show_hand1(self):
+        pass
 
-
-    a.setPlayer(0, 500)
-    a.setPlayer(1, 500)
-    a.setPlayer(2, 500)
-
-    a.setReady(0) # btn and utg
-    a.setReady(1) # sb
-    a.setReady(2) # bb
-
-    a.start()
-
-    # utg call
-    a.pcall(0)
-
-    # sb call
-    a.pcall(1)
-
-    # FLOP
-    a.praise(1, 40)
-    a.pcall(2)
-    a.pcall(0)
-
-    # TURN
-    a.pallin(1)
-    a.pcall(2) # will be allin
-    a.pcall(0) # wiil be allin
-
-    # RIVER
-
-    print('end')
+    def test_wrong_pos_take_action(self):
+        game = self.g
+        self.assertEqual(game.exe_pos, 1)
+        self.assertEqual(game.pfold(0), -1)
+        self.assertEqual(game.pfold(2), -1)
+        self.assertEqual(game.pfold(3), -1)
+        self.assertEqual(game.pfold(-1), -1)
+        self.assertEqual(game.pfold(1), 0)
+        self.assertEqual(game.exe_pos, 2)
