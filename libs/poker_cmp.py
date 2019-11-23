38c49bcf25485d35855c4f30dcc9f02fbc058d28
laazy <616408874@qq.com>
2019-11-23 08:41:42 +0000

    add test for poker_cmp

--- libs/poker_cmp.py
+++ libs/poker_cmp.py
@@ -1,9 +1,9 @@
 from itertools import combinations
 
 
-def card_ranks(cards):
+def _card_ranks(cards):
     '''
-        dfgsdgdfg
+        
     '''
     ranks = ['0123456789TJQKA'.index(r) for r, x in cards]
     ranks.sort(reverse=True)
@@ -12,63 +12,63 @@ def card_ranks(cards):
     return ranks
 
 
-def straight(ranks):
+def _straight(ranks):
     return (max(ranks) - min(ranks)) == 4 and len(set(ranks)) == 5
 
 
-def flush(cards):
+def _flush(cards):
     hand_flush = [s for r, s in cards]
     return len(set(hand_flush)) == 1
 
 
-def kind(n, ranks):
+def _kind(n, ranks):
     for r in ranks:
         if ranks.count(r) == n:
             return r
     return None
 
 
-def two_pair(ranks):
-    pair = kind(2, ranks)
-    lowpair = kind(2, list(reversed(ranks)))
+def _two_pair(ranks):
+    pair = _kind(2, ranks)
+    lowpair = _kind(2, list(reversed(ranks)))
     if pair and lowpair != pair:
         return list(map(int, (pair, lowpair)))
     else:
         return None
 
 
-def hand_rank(hand):
-    ranks = card_ranks(hand)
-    if straight(ranks) and flush(hand):
+def _hand_rank(hand):
+    ranks = _card_ranks(hand)
+    if _straight(ranks) and _flush(hand):
         return (8, max(ranks))
-    elif kind(4, ranks):
-        return (7, kind(4, ranks), kind(1, ranks))
-    elif kind(3, ranks) and kind(2, ranks):
-        return (6, kind(3, ranks), kind(2, ranks))
-    elif flush(hand):
+    elif _kind(4, ranks):
+        return (7, _kind(4, ranks), _kind(1, ranks))
+    elif _kind(3, ranks) and _kind(2, ranks):
+        return (6, _kind(3, ranks), _kind(2, ranks))
+    elif _flush(hand):
         return (5, ranks)
-    elif straight(ranks):
+    elif _straight(ranks):
         return (4, max(ranks))
-    elif kind(3, ranks):
-        return (3, kind(3, ranks), ranks)
-    elif two_pair(ranks):
-        return (2, two_pair(ranks), ranks)
-    elif kind(2, ranks):
-        return (1, kind(2, ranks), ranks)
+    elif _kind(3, ranks):
+        return (3, _kind(3, ranks), ranks)
+    elif _two_pair(ranks):
+        return (2, _two_pair(ranks), ranks)
+    elif _kind(2, ranks):
+        return (1, _kind(2, ranks), ranks)
     else:
         return (0, ranks)
 
 
 def poker7(cards):
     hands = combinations(cards, 5)
-    hand = max(hands, key=hand_rank)
-    return hand, hand_rank(hand)
+    hand = max(hands, key=_hand_rank)
+    return hand, _hand_rank(hand)
 
 
 if __name__ == '__main__':
     cards = "2s 3d 4s 5s As 9s Ts".split()
     print(poker7(cards))
     sf = "6c 7c 8c 9c Tc".split()
-    temp = hand_rank(sf)
+    temp = _hand_rank(sf)
     hands = [sf]
     res = poker7(hands)
---
+++ test/libs/test_poker_cmp.py
@@ -0,0 +1,21 @@
+import unittest
+from libs.poker_cmp import poker7
+
+TEST_CASE = [
+    # TODO: to be filled
+    ("2s 3d 4s 5s As 9s Ts", "4s 5s As 9s Ts"),
+    ("8s Kd Qs Js As 9s Ts", "Ts Js Qs Ks As"),
+    ("As 3d 5h 7d 2d Qs Kd", "5h 7d Qs Kd As"),
+    ("9s 5d Ts Th 4d Td 3d", "9s 5d Ts Th Td"),
+    ("2d Qh Ah 7s 2c Qc 8c", "2d Qh Ah 2c Qc"),
+]
+
+
+class TestPokerCmp(unittest.TestCase):
+    def assertUnorderListEqual(self, l1, l2, msg=None):
+        return self.assertSetEqual(set(l1), set(l2), msg)
+
+    def test_poker7(self):
+        for i in TEST_CASE:
+            h, r = poker7(i[0].split())
+            self.assertSetEqual(set(h), set(i[1].split()))
