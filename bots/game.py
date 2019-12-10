a7edef2d710bc98a5da4c6ef68b30c5606ef0549
Li Hao <yfzm1223@sjtu.edu.cn>
2019-12-10 19:51:14 +0800

    add command

--- bots/game.py
+++ bots/game.py
@@ -5,24 +5,11 @@ import logging
 from libs.manager import gameManager
 from slackapi.client import send_msg, send_private_msg_in_channel, update_msg, delete_msg, get_username
 from slackapi.payload import card_to_emoji
-from typing import Dict
+from typing import Dict, Callable
 
 
 logger = logging.getLogger(__name__)
-HELP_MSG = """Try commands below:
-`help` to print this msg,
-`open <name>(optional)` to create a table as <name>,
-`join <name>(optional)` to sit at a table as <name>,
-`reopen <name>(optional)` to reopen a table as <name>,
-`start` to start a game or continue next game,
-`continue` to continue next game,
-`leave` or `quit` to leave table,
-`bot` to ask a bot to join in,
-`chip` to check current chips you have.
-support the following poker operations:
-`bet <number>` for raise <number>,
-`call`, `all`, `check`, `fold`.
-"""
+HELP_MSG = ""
 
 
 class ChannelInfo:
@@ -31,59 +18,47 @@ class ChannelInfo:
         self.client = client
 
 
+class Command:
+    def __init__(self, pattern: str, func: Callable, name: str,
+                 description: str, need_text: bool = False, debug: bool = False):
+        self.pattern = pattern
+        self.func = func
+        self.name = name
+        self.description = description
+        self.need_text = need_text
+        self.debug = debug
+
+    def call(self, **args):
+        if not self.need_text:
+            args.pop("text")
+        self.func(**args)
+
+
 def handle_message(web_client: slack.WebClient, channel: str, user: str, ts: str, text: str, mentioned: bool):
-    def _get_username(s: str) -> str:
-        if len(s.split()) >= 2:
-            return " ".join(s.split()[1:])
-        else:
-            return get_username(web_client, user)
+    text = text.strip()
+    for command in commands:
+        if re.search(command.pattern, text, re.IGNORECASE) is not None:
+            command.call(web_client=web_client, channel=channel, user=user, text=text)
+            return
+    if mentioned:
+        echo_help(web_client, channel, user)
 
-    if re.search(r"^open((\s)+(\w)*)*$", text) is not None:
-        create_table(web_client, channel, user, _get_username(text))
-    elif re.search(r"^join((\s)+(\w)*)*$", text) is not None:
-        join_table(web_client, channel, user, _get_username(text))
-    elif re.search(r"^reopen((\s)+(\w)*)*$", text) is not None:
-        reopen_table(web_client, channel, user, _get_username(text))
-    elif text == "start":
-        start_game(web_client, channel, user)
-    elif re.search(r"^bet(\s)+(\d)+$", text) is not None:
-        chip = int(text.split()[1])
-        bet(web_client, channel, user, chip)
-    elif text == "call":
-        call(web_client, channel, user)
-    elif text == "all":
-        all_in(web_client, channel, user)
-    elif text == "check":
-        check(web_client, channel, user)
-    elif text == "fold":
-        fold(web_client, channel, user)
-    elif text == "continue":
-        start_game(web_client, channel, user)
-    elif text == "leave" or text == "quit":
-        leave_table(web_client, channel, user)
-    elif text == "info":
-        echo_info(web_client, channel)
-    elif text == "bot":
-        add_bot(web_client, channel)
-    elif text == "make me rich":
-        gain_chip(web_client, channel, user)
-    elif text == "chip":
-        show_chip(web_client, channel, user)
+
+def _get_username(s: str, web_client: slack.WebClient, userid: str) -> str:
+    if len(s.split()) >= 2:
+        return " ".join(s.split()[1:])
     else:
-        if mentioned or text == "help":
-            send_msg(web_client, channel, HELP_MSG, user)
+        return get_username(web_client, userid)
 
 
-channels: Dict[str, ChannelInfo] = dict()
-
-
-def create_table(web_client: slack.WebClient, channel: str, user: str, username: str):
+def create_table(web_client: slack.WebClient, channel: str, user: str, text: str):
     if channel in channels.keys():
         send_msg(web_client, channel,
                  "Failed to open a game, because there is an unfinished game in this channel! "
                  "If you want to reopen the table, type `reopen` instead")
         return
 
+    username = _get_username(text, web_client, user)
     table_id = gameManager.open(user)
     channels[channel] = ChannelInfo(table_id, web_client)
     send_msg(web_client, channel,
@@ -91,7 +66,8 @@ def create_table(web_client: slack.WebClient, channel: str, user: str, username:
     join_table(web_client, channel, user, username)
 
 
-def reopen_table(web_client: slack.WebClient, channel: str, user: str, username: str):
+def reopen_table(web_client: slack.WebClient, channel: str, user: str, text: str):
+    username = _get_username(text, web_client, user)
     if channel not in channels.keys():
         send_msg(web_client, channel,
                  "Warning: There is no opened table in this channel. Create table instead.")
@@ -105,8 +81,8 @@ def reopen_table(web_client: slack.WebClient, channel: str, user: str, username:
     create_table(web_client, channel, user, username)
 
 
-def join_table(web_client: slack.WebClient, channel: str, user: str, username: str):
-    # TODO: use wrapper to check channel
+def join_table(web_client: slack.WebClient, channel: str, user: str, text: str):
+    username = _get_username(text, web_client, user)
     if channel not in channels.keys():
         send_msg(web_client, channel,
                  "Failed to join the table, because there is no opened game in this channel.")
@@ -135,12 +111,12 @@ def leave_table(web_client: slack.WebClient, channel: str, user: str):
                  "Failed to join the table, because there is no opened game in this channel.")
         return
     table_id = channels[channel].table_id
-    nplayer, err = gameManager.leave(table_id, user)
+    nplayers, err = gameManager.leave(table_id, user)
     if err is not None:
         send_msg(web_client, channel, err)
         return
 
-    send_msg(web_client, channel, f"just leaf the table, total player: {nplayer}", user)
+    send_msg(web_client, channel, f"just leaf the table, total player: {nplayers}", user)
 
 
 def start_game(web_client: slack.WebClient, channel: str, user: str):
@@ -167,7 +143,7 @@ def start_game(web_client: slack.WebClient, channel: str, user: str):
              "Game started! I have send your hand to you personally. And type help or @me to get help message")
 
 
-def add_bot(web_client: slack.WebClient, channel: str):
+def add_bot(web_client: slack.WebClient, channel: str, user: str):
     if channel not in channels.keys():
         send_msg(web_client, channel,
                  "Failed to continue, because there is no opened game in this channel.")
@@ -179,16 +155,17 @@ def add_bot(web_client: slack.WebClient, channel: str):
         return
 
 
-def bet(web_client: slack.WebClient, channel: str, user: str, chip):
+def bet(web_client: slack.WebClient, channel: str, user: str, text: str):
+    chip = int(text.split()[-1])
     table_id = channels[channel].table_id
-    err = gameManager.bet(table_id, user, int(chip))
+    err = gameManager.bet(table_id, user, chip)
     if err is not None:
         send_msg(web_client, channel, err)
 
 
-def call(web_client: slack.WebClient, channel: str, user: str):
+def call_or_check(web_client: slack.WebClient, channel: str, user: str):
     table_id = channels[channel].table_id
-    err = gameManager.call(table_id, user)
+    err = gameManager.call_or_check(table_id, user)
     if err is not None:
         send_msg(web_client, channel, err)
 
@@ -200,13 +177,6 @@ def all_in(web_client: slack.WebClient, channel: str, user: str):
         send_msg(web_client, channel, err)
 
 
-def check(web_client: slack.WebClient, channel: str, user: str):
-    table_id = channels[channel].table_id
-    err = gameManager.check(table_id, user)
-    if err is not None:
-        send_msg(web_client, channel, err)
-
-
 def fold(web_client: slack.WebClient, channel: str, user: str):
     table_id = channels[channel].table_id
     err = gameManager.fold(table_id, user)
@@ -214,9 +184,34 @@ def fold(web_client: slack.WebClient, channel: str, user: str):
         send_msg(web_client, channel, err)
 
 
-def echo_info(web_client: slack.WebClient, channel: str):
+def echo_info(web_client: slack.WebClient, channel: str, user: str):
     table_id = channels[channel].table_id
     send_msg(web_client, channel, gameManager.get_game_info(table_id))
+
+
+def gain_chip(web_client: slack.WebClient, channel: str, user: str):
+    err = gameManager.gain_chip(user)
+    if err is None:
+        send_msg(web_client, channel, ", you get $500!", user)
+    else:
+        send_msg(web_client, channel, err, user)
+
+
+def show_chip(web_client: slack.WebClient, channel: str, user: str):
+    chip, err = gameManager.show_chip(user)
+    if err is None:
+        send_msg(web_client, channel, f", you have ${chip}", user)
+    else:
+        send_msg(web_client, channel, err, user)
+
+
+def echo_help(web_client: slack.WebClient, channel: str, user: str):
+    global HELP_MSG
+    if HELP_MSG == "":
+        for command in commands:
+            if not command.debug:
+                HELP_MSG += f"`{command.name}`: {command.description}\n"
+    send_msg(web_client, channel, HELP_MSG)
 
 
 def send_to_channel_by_table_id(table_id, msg="void", blocks=None):
@@ -260,17 +255,22 @@ def delete_msg_by_table_id(table_id, ts):
     return "table_id not found"
 
 
-def gain_chip(web_client: slack.WebClient, channel: str, user: str):
-    err = gameManager.gain_chip(user)
-    if err is None:
-        send_msg(web_client, channel, ", you get $500!", user)
-    else:
-        send_msg(web_client, channel, err, user)
+channels: Dict[str, ChannelInfo] = dict()
 
-
-def show_chip(web_client: slack.WebClient, channel: str, user: str):
-    chip, err = gameManager.show_chip(user)
-    if err is None:
-        send_msg(web_client, channel, f", you have ${chip}", user)
-    else:
-        send_msg(web_client, channel, err, user)
+commands = (
+    Command(r"^open((\s)+(\w)*)*$", create_table, "open <name>", "open a table", need_text=True),
+    Command(r"^join((\s)+(\w)*)*$", join_table, "join <name>", "join a table", need_text=True),
+    Command(r"^reopen((\s)+(\w)*)*$", reopen_table, "reopen <name>", "reopen a table", need_text=True),
+    Command(r"^leave$", leave_table, "leave", "leave the table"),
+    Command(r"^start$", start_game, "start", "start a game or continue the game"),
+    Command(r"^(b(et)?(\s)+)?(\d)+$", bet, "<bet> num", "bet chips", need_text=True),
+    Command(r"^c(all)?$", call_or_check, "c<all>", "call"),
+    Command(r"^c(heck)?$", call_or_check, "c<heck>", "check"),
+    Command(r"^a(ll)?$", all_in, "a<ll>", "all in"),
+    Command(r"^f(old)?$", fold, "f<old>", "fold"),
+    Command(r"^bot$", add_bot, "bot", "add a bot to the table"),
+    Command(r"^chip$", show_chip, "chip", "show how many chips you have"),
+    Command(r"^help$", echo_help, "help", "get help message"),
+    Command(r"^info$", echo_info, "info", "show internal state of the current game", debug=True),
+    Command(r"^make me rich$", gain_chip, "make me rich", "gain more chips", debug=True)
+)
