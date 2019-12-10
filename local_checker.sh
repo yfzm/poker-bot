43c4625358ebb699f6a18942bb195d74e0b46607
Li Hao <yfzm1223@sjtu.edu.cn>
2019-12-10 21:11:44 +0800

    update flake8

--- local_checker.sh
+++ local_checker.sh
@@ -1,6 +1,6 @@
 set -e
 
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 . --count --ignore=C901 --max-complexity=10 --max-line-length=127 --statistics
+flake8 . --count --max-complexity=10 --max-line-length=127 --statistics
 
 python poker_test.py
